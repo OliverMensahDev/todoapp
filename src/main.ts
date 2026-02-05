fae4ffbf5ecfe054d1c2aa7e833b0a847f9d9400
Oliver Mensah <olivermensah96@gmail.com>
2026-02-05 05:42:39 +0000

    using env variables

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import "dotenv/config";
 
 import { MySQLTodoRepository } from "./infrastructure/repositories/MySQLTodoRepository";
 import { CreateTodo } from "./application/usecases/CreateTodo";
